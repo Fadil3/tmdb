7aea691c283e46f63ed2ed4029eab7525429a6ef
Fadil <mrayhanfadil@upi.edu>
2024-10-14 21:47:00 +0700

    feat(route): add basic route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+
+import Root from './routes/Root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    index: true,
+    element: <Root />,
+  },
+  {
+    path: "/watchlists",
+    element: <div>watchlists!</div>,
+  },
+  {
+    path: "/favorites",
+    element: <div>favorites</div>,
+  },
+  {
+    path: "/movies",
+    element: <div>list movies</div>,
+  },
+  {
+    path: "/movie/:id",
+    element: <div>Detail movie</div>,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
